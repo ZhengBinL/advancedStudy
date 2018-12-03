4de19b54e4125696cec1c276443ab6ebebe041a3
zhengbinlong <yun.qian>
2018-12-03 10:19:01 +0800

    start

---
+++ angular/my-app/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({//装饰器
+  declarations: [//该模块所拥有的组件
+    AppComponent
+  ],
+  imports: [//该模块依赖的其他模块
+    BrowserModule
+  ],
+  providers: [],//该模块所拥有的服务提供商
+  bootstrap: [AppComponent]//指定跟组件，只有根模块需要该配置项Angular创建它并插入index.html宿主页面
+//exports:[]  公开该模块其中的一部分，以便外部模块使用它们
+})
+export class AppModule { }
