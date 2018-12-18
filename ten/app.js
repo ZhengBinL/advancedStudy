b28eae7891ed9c4d2b9e10b2ff3a2c1d33b73116
ZhengBinL <774037817@qq.com>
2018-12-18 17:41:37 +0800

    first

---
+++ ten/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
+
+const app = express()
+
+//开放public目录资源
+app.use('/public/',express.static(path.join(__dirname,'./public/')))
+
+nunjucks.configure(path.join(__dirname,'./view'), {
+    autoescape: true,
+    express: app,
+    watch:true//禁用模版文件缓存
+})
+
+app.get('/', (req, res, next) => {
+    // res.status(200).send('hello')
+    res.render('index.html')
+})
+
+app.listen(3000, () => {
+    console.log("服务启动成功.")
+    console.log('http://localhost:3000/')
+})
